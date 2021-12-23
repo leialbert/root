f00d5b81be1da3a85b4bb0f11c26d7db9d29d3e1
Albert Lei <albert.lei@hotmail.com>
2021-12-23 21:10:33 +0300

    create models for Question and Choices

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import DateTimeField, TextField
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
