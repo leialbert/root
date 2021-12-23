6206fd7d417c1d80e20a5ae3522ed7ff0a5681d3
Albert Lei <albert.lei@hotmail.com>
2021-12-23 22:37:47 +0300

    shortcut of Http404

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http.response import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 
 # Create your views here.
@@ -16,10 +16,11 @@ def index(request):
 
 
 def detail(request,question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist')
+    question = get_object_or_404(Question,pk=question_id)
     context = {
         'question':question,
     }
