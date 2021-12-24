4d820765f54ed2fac946713cfebb3b2338b812cb
Albert Lei <albert.lei@hotmail.com>
2021-12-24 03:02:05 +0300

    add some filters

--- polls/models.py
+++ polls/models.py
@@ -4,11 +4,18 @@ from django.db.models.fields import CharField, DateTimeField, IntegerField
 from django.db.models.fields.related import ForeignKey
 import datetime
 from django.utils import timezone
+from django.contrib import admin
 class Question(models.Model):
     question_text = CharField(max_length=200)
     pub_date = DateTimeField('date published')
     def __str__(self):
         return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
